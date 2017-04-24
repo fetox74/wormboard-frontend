72874a5e5068da5aa2d74141381db5064911623a
Oliver Dittrich <odi@conleos.com>
2017-04-24 18:26:41 +0200

    first rudimentary implementation of the menubar (aka month can now trigger the fetching of new datasets, still has to be preceded by a initial call to get for which month and years data is actually present)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,91 @@ import {ZKBAggregate} from './model/zkb-aggregate';
   providers: [AggregateService]
 })
 export class AppComponent implements OnInit {
-  aggregates: ZKBAggregate[];
+  public aggregates: ZKBAggregate[];
+  public selected: string;
 
   constructor(private aggregateService: AggregateService) {
   }
 
   ngOnInit() {
-    /*this.aggregateService.getStatsForMonth('201701').first().subscribe(e => {
-      this.aggregates = e;
-    });*/
+    this.selected = 'ALL';
     this.aggregateService.getStatsForYear('2017').first().subscribe(e => {
       this.aggregates = e;
     });
+  }
+
+  onSelect(month: string) {
+    if (month !== this.selected) {
+      this.selected = month;
+      switch (month) {
+        case 'ALL':
+          this.aggregateService.getStatsForYear('2017').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Jan':
+          this.aggregateService.getStatsForMonth('201701').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Feb':
+          this.aggregateService.getStatsForMonth('201702').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Mar':
+          this.aggregateService.getStatsForMonth('201703').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Apr':
+          this.aggregateService.getStatsForMonth('201704').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'May':
+          this.aggregateService.getStatsForMonth('201705').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Jun':
+          this.aggregateService.getStatsForMonth('201706').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Jul':
+          this.aggregateService.getStatsForMonth('201707').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Aug':
+          this.aggregateService.getStatsForMonth('201708').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Sep':
+          this.aggregateService.getStatsForMonth('201709').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Oct':
+          this.aggregateService.getStatsForMonth('201710').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Nov':
+          this.aggregateService.getStatsForMonth('201711').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        case 'Dec':
+          this.aggregateService.getStatsForMonth('201712').first().subscribe(e => {
+            this.aggregates = e;
+          });
+          break;
+        default:
+          break;
+      }
+    }
   }
 }
